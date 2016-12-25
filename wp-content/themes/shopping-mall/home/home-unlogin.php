7edf6b540edff62ea004efa9ac81f4ebfc99fa29
binhdarkcu <binhdarkcu@gmail.com>
2016-12-25 23:00:03 +0700

    style home

---
+++ wp-content/themes/shopping-mall/home/home-unlogin.php
@@ -0,0 +1,65 @@
+<div class="section section--low-poly">
+   <div class="container">
+      <h2 class="section__title">VR / AR Ready Low-poly 3D Models</h2>
+      <div class="section__content">
+         <div class="section__text">
+            <h3 class="title">Low Poly 3D Models ready for video games, VR &amp; AR real-time applications</h3>
+            <a class="button button--big button--full button--big-text" href="https://www.cgtrader.com/lowpoly-3d-models" id="87f016e909dc553639037de2b341e40f">Low poly 3D models</a>
+         </div>
+      </div>
+   </div>
+</div>
+
+<div class="section section--alt section--cg-models">
+   <div class="container">
+      <h2 class="section__title">3D Models</h2>
+      <div class="section__content">
+         <div class="section__text">
+            <h3 class="title">Buy professional 3D Models for your project</h3>
+            <a class="button button--big button--full button--big-text" href="https://www.cgtrader.com/3d-models" id="46ff685891ca1cb2674cbaa12c238c5f">Buy 3D models</a>
+         </div>
+      </div>
+   </div>
+</div>
+<div class="section section--dark section--jobs js-bg is-loaded" style="background: url(<?php echo get_template_directory_uri() ?>/images/header_bg.jpg) no-repeat;    background-size: cover; position: relative; z-index: 0;">
+   <div class="container">
+      <h2 class="section__title">3D Jobs</h2>
+      <div class="section__content">
+         <div class="jobs-content">
+            <div class="jobs-content__find">
+               <h3 class="title">Find 3D related jobs and sell your skills</h3>
+            </div>
+            <div class="jobs-content__hire">
+               <h3 class="title">Hire freelancers to help finish your projects in time</h3>
+            </div>
+         </div>
+      </div>
+      <p><a class="button button--big button--big-text button--longer" href="#">Post or find 3D jobs</a></p>
+   </div>
+</div>
+<div class="section section--community">
+   <div class="container">
+      <h2 class="section__title">3D Designer Community</h2>
+      <div class="section__content">
+         <div class="community-content">
+            <div class="community-content__col community-content__col--designers">
+               <h3 class="community-content__title">Designers</h3>
+               <p class="title">Build your portfolio and become the best among 3D designers</p>
+            </div>
+            <div class="community-content__col community-content__col--challenges">
+               <h3 class="community-content__title">Challenges</h3>
+               <p class="title">Participate in challenges and win hefty prizes</p>
+            </div>
+            <div class="community-content__col community-content__col--galleries">
+               <h3 class="community-content__title">Galleries</h3>
+               <p class="title">Showcase your best renders, get feedback and recognition</p>
+            </div>
+            <div class="community-content__col community-content__col--forum">
+               <h3 class="community-content__title">Forum</h3>
+               <p class="title">Find tutorials and discuss other 3d related matters</p>
+            </div>
+         </div>
+      </div>
+      <p><a class="button button--big button--big-text button--longer" href="https://www.cgtrader.com/3d-community" id="eae20b6f120267b2675a8f586425f8cf">Join the Community!</a></p>
+   </div>
+</div>
