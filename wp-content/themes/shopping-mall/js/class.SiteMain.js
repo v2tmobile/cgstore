8ef32888a280c6c6e0ee7ab396a020bc3576157a
binhdarkcu <binhdarkcu@gmail.com>
2016-12-27 23:03:43 +0700

    style

---
+++ wp-content/themes/shopping-mall/js/class.SiteMain.js
@@ -0,0 +1,51 @@
+// JavaScript Document
+var SiteMain = (function() {
+	function init(){
+		createTooltip();
+	}
+	
+	function createRadio(){
+		$('input.iCheckRadio').iCheck();
+	}
+	
+	function createTooltip(){
+		$(".tooltip").each(function(){
+			
+			$(this).tooltipster({
+				contentCloning: true,
+				side: 'right',
+				contentAsHTML: true,
+				functionReady: function(){
+					$('.tooltipster-box .slick').not('.slick-initialized').slick({
+						dots: false,
+						    prevArrow: false,
+						    nextArrow: false
+
+					});
+				}
+			});
+			
+		});
+		
+	}
+	
+	function openPopup(idDiv){
+		$('.result_question').css('display','none')
+		$(idDiv).css('display','block');	
+	}
+	function closePopup(idDiv){
+		$(idDiv).css('display','none');
+        return false;
+	}
+	return {
+		init:init,
+		openPopup:openPopup,
+		closePopup:closePopup
+	}
+	
+})();		
+
+$(document).ready( function() {
+	SiteMain.init();
+});
+
