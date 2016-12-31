a508162d94d70da512554f26854e269c39a6f951
binhdarkcu <binhdarkcu@gmail.com>
2016-12-31 15:07:03 +0700

    dsad

---
+++ wp-content/themes/shopping-mall/home/socials.php
@@ -0,0 +1,38 @@
+<ul class="list list-inline">
+	<li class="list-item">
+		<a href="#" target="_blank" class="social-icon facebook">
+			<span class="fa-stack fa-lg fa-facebook-icon"><i class="fa fa-circle fa-stack-2x"></i><i
+					class="fa fa-facebook fa-inverse fa-stack-1x"></i></span>
+		</a>
+	</li>
+	<li class="list-item">
+		<a href="#" target="_blank" class="social-icon linkedin">
+			<span class="fa-stack fa-lg fa-linkedin-icon"><i class="fa fa-circle fa-stack-2x"></i><i
+					class="fa fa-linkedin fa-inverse fa-stack-1x"></i></span>
+		</a>
+	</li>
+	<li class="list-item">
+		<a href="#" target="_blank" class="social-icon twitter">
+			<span class="fa-stack fa-lg fa-twitter-icon"><i class="fa fa-circle fa-stack-2x"></i><i
+					class="fa fa-twitter fa-inverse fa-stack-1x"></i></span>
+		</a>
+	</li>
+	<li class="list-item">
+		<a href="#" target="_blank" class="social-icon instagram">
+			<span class="fa-stack fa-lg fa-instagram-icon"><i class="fa fa-circle fa-stack-2x"></i><i
+					class="fa fa-instagram fa-inverse fa-stack-1x"></i></span>
+		</a>
+	</li>
+	<li class="list-item">
+		<a href="#" target="_blank" class="social-icon pinterest">
+			<span class="fa-stack fa-lg fa-pinterest-icon"><i class="fa fa-circle fa-stack-2x"></i><i
+					class="fa fa-pinterest fa-inverse fa-stack-1x"></i></span>
+		</a>
+	</li>
+	<li class="list-item is-last">
+		<a href="#" target="_blank" class="social-icon google-plus">
+			<span class="fa-stack fa-lg fa-google-plus-icon"><i class="fa fa-circle fa-stack-2x"></i><i
+					class="fa fa-google-plus fa-inverse fa-stack-1x"></i></span>
+		</a>
+	</li>
+</ul>
--- wp-content/themes/shopping-mall/single-challenge.php
+++ wp-content/themes/shopping-mall/single-challenge.php
@@ -2,17 +2,48 @@
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			
 		<?php
 		while ( have_posts() ) : the_post();
-              
-             the_content();
-			
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+		?>
+		<div class="challenge-header">
+			<?php echo get_the_post_thumbnail(get_the_ID());?>
+		</div>
+		<div class="challenge-content">
+			<h1 class="challenge-content__title"><?php the_title();?></h1>
+			<div class="challenge-content__description">
+				<?php the_content();?>
+			</div>
+			<div class="challenge-content__subtitle">Participation</div>
+			<div class="challenge-info">
+			   <div class="challenge-info__action"><a class="button button--alt button--material js-publisher-trigger js-auth-control" id="" href="javascript:;">Upload 3D Model</a></div>
+			   <div class="challenge-info__countdown">
+			      <div class="countdown js-countdown" data-timestamp="2017-01-23 23:59:59 UTC">
+			         <div class="countdown__block countdown__months">0M</div>
+			         <div class="countdown__block countdown__days">23d</div>
+			         <div class="countdown__block countdown__hours">16h</div>
+			         <div class="countdown__block countdown__minutes">22m</div>
+			         <div class="countdown__block countdown__seconds">57s</div>
+			      </div>
+			   </div>
+			   <div class="challenge-info__deadline">Ends in: <b>24 days</b> (2017-01-23 23:59:59 UTC) </div>
+			   <div class="clear"></div>
+			</div>
+			<div class="challenge__share">
+			   <span>Share challenge:</span>
+			   <?php get_template_part('home/socials')?>
+			</div>
+			<div class="yourcomment">
+				<h3>Post a comment</h3>
+			<?php
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+			</div>
+		</div>
 
+		<?php
 		endwhile; // End of the loop.
 		?>
 
