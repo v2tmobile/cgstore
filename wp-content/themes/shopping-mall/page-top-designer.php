d5368b6611832ba6c4dcec4431150c269594fd44
binhdarkcu <binhdarkcu@gmail.com>
2017-01-05 00:18:23 +0700

    dsada

---
+++ wp-content/themes/shopping-mall/page-top-designer.php
@@ -0,0 +1,111 @@
+<?php
+/* Template Name: Top Designer Page */
+/**
+ * @package Shopping_Mall
+ */
+
+get_header(); ?>
+
+<div class="forum-page">
+	<div class="content-area">
+		<div class="breadcrumb-wrapper" id="breadcrumb">
+			<ul class="breadcrumb" itemscope="itemscope" itemtype="https://schema.org/BreadcrumbList">
+               <li class="breadcrumb-item" itemprop="itemListElement" itemscope="itemscope" itemtype="http://schema.org/ListItem">
+	                <a href="http://localhost/cgstore" itemprop="item" itemscope="itemscope" itemtype="http://schema.org/Thing" title="Home">
+	                    <span itemprop="name">Home</span>
+	                </a>
+	                <meta content="1" itemprop="position">
+            	</li>
+                <li class="breadcrumb-item" itemprop="itemListElement" itemscope="itemscope" itemtype="http://schema.org/ListItem">
+		            <span itemprop="item" itemscope="itemscope" itemtype="http://schema.org/Thing">
+		              <span itemprop="name">Forum</span>
+		            </span>
+		                <meta content="2" itemprop="position">
+		        </li>
+            </ul>
+		</div>
+
+		<div class="content-heading">
+		   <h2 class="content-heading__title">Top Designers</h2>
+		   <div class="content-heading__subtitle">Below is the list of top 3D designers on CGTrader. Their reputation score represents their activity on CGTrader - including the number of 3D models uploaded, sales, downloads, comments and rating of their work. CGTrader is a designer-friendly 3D model marketplace which aims to democratize 3D model market and provide the best conditions for 3D designers to buy, sell and share 3D models.</div>
+		</div>
+		
+		<div class="users-search">
+		   <form action="/designers" accept-charset="UTF-8" method="get">
+		      <div class="users-search__field"><input type="text" name="keywords" id="keywords" class="field field--full" placeholder="Search by username or full name"></div>
+		      <div class="users-search__sort filterSelect">
+		         <select name="sort_by" id="sort_by" class="select">
+		            <option value="relevance">Relevance</option>
+		            <option value="reputation">Reputation</option>
+		        </select>
+		      </div>
+		      <div class="users-search__action"><button name="" type="submit" class="button button--full">Search</button></div>
+		      <div class="clear"></div>
+		   </form>
+		</div>
+
+		<div class="users-list">
+			<div class="box user tooltip product-grid-item" data-toggle="tooltip" >
+				<input type="hidden" class="tooltip-data" value="[&quot;http:\/\/localhost:8080\/CGStore\/github\/wp-content\/uploads\/2016\/12\/1-218x300.jpg&quot;,&quot;http:\/\/localhost:8080\/CGStore\/github\/wp-content\/uploads\/2016\/12\/2-218x300.jpg&quot;,&quot;http:\/\/localhost:8080\/CGStore\/github\/wp-content\/uploads\/2016\/12\/3-218x300.jpg&quot;,&quot;http:\/\/localhost:8080\/CGStore\/github\/wp-content\/uploads\/2016\/12\/4-218x300.jpg&quot;]">
+				<div class="l-inner">
+					<div class="user__header">
+					   <div class="user__rank"><span class="js-tooltip tooltipstered"># 1</span></div>
+					   <div class="user__avatar">
+					      <div class="avatar"><a id="92578c3b67f33dcf1cdea3cdddfcebfd" href="https://www.cgtrader.com/squir"><img alt="squir" src="https://img-new.cgtrader.com/avatars/2159/large_f91c255e-cc12-41d3-99a1-156a9e5373cc.png"></a></div>
+					   </div>
+					   <div class="user__name">
+					      <h4><a id="" href="https://www.cgtrader.com/squir">squir</a></h4>
+					   </div>
+					   <div class="user__tags">
+					      <div class="tag-list">
+					         <ul>
+					            <li><a id="" href="https://www.cgtrader.com/3d-models/car">car</a></li>
+					            <li><a id="" href="https://www.cgtrader.com/3d-models/ext/fbx">fbx</a></li>
+					            <li><a id="" href="https://www.cgtrader.com/3d-models/2015">2015</a></li>
+					            <li><a id="" href="https://www.cgtrader.com/3d-models/2016">2016</a></li>
+					            <li><a id="" href="https://www.cgtrader.com/3d-models/sport">sport</a></li>
+					         </ul>
+					      </div>
+					   </div>
+					   <div class="clear"></div>
+					</div>
+					<div class="user__info">
+					   <ul class="list list--inline">
+					      <li>Models: <b>3,351</b></li>
+					      <li>Profile Views: <b>4.86m</b></li>
+					      <li>Reputation: <b>338,936</b></li>
+					   </ul>
+					</div>
+					<div class="user__items">
+						<div class="items-list">
+							<div class="content-box-wrapper grid grid--dense">
+								<div class="grid__col">
+								   <div class="content-box content-box--compact content-box--interactive tooltipstered" data-list="Designers Page">
+								      <div class="content-box__content" data-brand="squir" data-category="Car" data-id="18210" data-name="Skoda Octavia RS combi 2014" data-price="128.0" data-variant="CG">
+								         <a title="Skoda Octavia RS combi 2014 3D Model" id="_designers_skoda_octavia_rs_combi_2014" class="content-box__link js-content-box-item" href="https://www.cgtrader.com/3d-models/car/standard/skoda-octavia-rs-combi-2014">
+								            <figure class="content-box__image js-carousel-items" data-images="[&quot;https://img2.cgtrader.com/items/18210/8237f9b87d/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;, &quot;https://img-new.cgtrader.com/items/18210/2863cfc9ae/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;, &quot;https://img2.cgtrader.com/items/18210/387ffb4ca6/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;, &quot;https://img-new.cgtrader.com/items/18210/08f2151391/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;, &quot;https://img2.cgtrader.com/items/18210/f448edecd4/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;, &quot;https://img-new.cgtrader.com/items/18210/4f0d0f3986/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;, &quot;https://img2.cgtrader.com/items/18210/d6e535214b/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;, &quot;https://img-new.cgtrader.com/items/18210/65cbc006c9/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;, &quot;https://img-new.cgtrader.com/items/18210/bb35e7a216/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;, &quot;https://img2.cgtrader.com/items/18210/ceeed52be6/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;, &quot;https://img1.cgtrader.com/items/18210/6b97f6f9b8/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;, &quot;https://img-new.cgtrader.com/items/18210/371255d193/large/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg&quot;]">
+								               <img alt="" src="https://img1.cgtrader.com/items/18210/8237f9b87d/grid/skoda-octavia-rs-combi-2014-3d-model-max-obj-3ds-fbx-c4d-lwo-lw-lws.jpg">
+								               <figcaption class="content-box__title">Skoda Octavia RS combi 2014</figcaption>
+								            </figure>
+								         </a>
+								      </div>
+								      <div class="content-box__price" data-price="$128.00">$128.00</div>
+								      <div class="content-box__controls">
+								         <div class="content-box__control js-carousel-trigger" data-direction="prev"><button class="action"><i class="fa fa-chevron-left fa-lg"></i></button></div>
+								         <div class="content-box__control js-carousel-trigger" data-direction="next"><button class="action"><i class="fa fa-chevron-right fa-lg"></i></button></div>
+								      </div>
+								   </div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+
+		
+	</div>
+</div>
+
+<?php get_footer(); ?>
