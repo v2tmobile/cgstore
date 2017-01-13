fb36e80a6894e9eb64d49fe49ab3ba1b44ed4464
TUNG KEN <TUNG KEN>
2017-01-13 21:26:11 +0700

    post product

--- wp-content/themes/shopping-mall/inc/ajax.php
+++ wp-content/themes/shopping-mall/inc/ajax.php
@@ -22,3 +22,33 @@ function ats_unlike_all(){
     echo wp_send_json($result);
     die(); 
 }
+
+// load sub cat
+
+add_action( 'wp_ajax_nopriv_cgs-load-sub-cat', 'cgs_load_sub_cat' );
+add_action( 'wp_ajax_cgs-load-sub-cat', 'cgs_load_sub_cat' );
+
+function cgs_load_sub_cat(){
+   $result = array('success'=>false,'msg'=>'Error');
+   $catID = isset($_REQUEST['catID']) ? $_REQUEST['catID']: 0;
+   $tax = 'product_cat';
+   $hmtl = '';
+   if($catID){
+       $cats = get_terms($tax,array(
+           'hide_empty'=>0,
+           'parent'=>$catID
+        ));
+     
+      if($cats){
+        foreach ($cats as $cat) {
+          $hmtl .='<option value="'.$cat->term_id.'">'.$cat->name.'</option>';
+        }
+      } 
+     $result['success'] = true;
+     $result['data'] = $hmtl;  
+   }
+
+   echo wp_send_json($result);
+    die(); 
+
+}
