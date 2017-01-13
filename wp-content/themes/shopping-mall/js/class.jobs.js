fd0e3bf4f5ce19de8a1b2e02de9248021719d1ed
binhdarkcu <binhdarkcu@gmail.com>
2017-01-13 15:41:43 +0700

    dsa

--- wp-content/themes/shopping-mall/js/class.jobs.js
+++ wp-content/themes/shopping-mall/js/class.jobs.js
@@ -1,6 +1,9 @@
 // JavaScript Document
 var Jobs = (function() {
 	function init(){
+		galleries_upload();
+		initSelectGallery();
+		galleryTags();
 	}
 
 	function openPopup(idDiv){
@@ -10,6 +13,26 @@ var Jobs = (function() {
 	function closePopup(idDiv){
 		$(idDiv).css('display','none');
         return false;
+	}
+	function initSelectGallery(){
+		$('select.select').select2({
+			minimumResultsForSearch: -1
+		});
+	}
+	function galleries_upload(){
+		$('#fileUploadGallery').MultiFile({
+	    	accept: 'jpg|png|gif',
+	    	list: '#fileGallery',
+	    	onFileAppend: function(){
+	    		$('#gallery-form-container').show();
+	    	},
+	    	afterFileRemove: function(){
+	    		$('#gallery-form-container').hide();	
+	    	}
+	    });
+	}
+	function galleryTags(){
+		$('#gallery_tags').tagsInput({width:'auto'});
 	}
 	return {
 		init:init,
