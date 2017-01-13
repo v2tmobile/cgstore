a740fbfdf310240a539a295c46b8804cdde4ce0b
binhdarkcu <binhdarkcu@gmail.com>
2017-01-13 23:35:38 +0700

    dsa

--- wp-content/themes/shopping-mall/wc-vendors/dashboard/product-edit.php
+++ wp-content/themes/shopping-mall/wc-vendors/dashboard/product-edit.php
@@ -402,7 +402,41 @@ html{
 <script type="text/javascript">
 
  jQuery(document).ready(function($){
-   
+
+
+   /*$("#wcv-product-edit").validate({
+		onkeyup: false,
+        errorClass: "error",
+
+        //put error message behind each form element
+        errorPlacement: function (error, element) {
+            var elem = $(element);
+            error.insertAfter(element);
+        },
+
+        //When there is an error normally you just add the class to the element.
+        // But in the case of select2s you must add it to a UL to make it visible.
+        // The select element, which would otherwise get the class, is hidden from
+        // view.
+        highlight: function (element, errorClass, validClass) {
+            var elem = $(element);
+            if (elem.hasClass("select2-offscreen")) {
+                $("#s2id_" + elem.attr("id") + " ul").addClass(errorClass);
+            } else {
+                elem.addClass(errorClass);
+            }
+        },
+
+        //When removing make the same adjustments as when adding
+        unhighlight: function (element, errorClass, validClass) {
+            var elem = $(element);
+            if (elem.hasClass("select2-offscreen")) {
+                $("#s2id_" + elem.attr("id") + " ul").removeClass(errorClass);
+            } else {
+                elem.removeClass(errorClass);
+            }
+        }
+	});*/
    $('#product-cat-parent').on("change", function(e) {
    	  var catid = $(this).val();
    	  if(catid && catid !== 'undefined'){
