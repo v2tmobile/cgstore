cb5e787d0b246c1288687a22c2b45907122fc729
binhdarkcu <binhdarkcu@gmail.com>
2017-02-10 12:52:30 +0700

    sda

--- wp-content/themes/shopping-mall/page-referrals.php
+++ wp-content/themes/shopping-mall/page-referrals.php
@@ -1,13 +1,64 @@
 <?php get_header(); ?>
 
 
-	<div class="content-area site-page--gallery">
+	<div class="content-area site-page--gallery site-page--referrals ">
 	  	<section class="content-section blog-post">
 			<div class="container">
 				<div class="content-heading"><h2 class="content-heading__title">Referrals</h2><p class="content-heading__subtitle">Our referral system allows you to invite people to CGTrader. The more people register through you, the more bonus reputation points you get! </p></div>
+				<div class="referral-blocks">
+				   <div class="referral-block">
+				      <div class="referral-block__image"><img alt="Icon" src="<?php echo TEMPLATE_PATH; ?>/images/Web_essentials_part2.svg"></div>
+				      <h3 class="referral-block__title">Invite</h3>
+				      <p class="referral-block__description">Send invites or share your referral link</p>
+				   </div>
+				   <div class="referral-block">
+				      <div class="referral-block__image"><img alt="Icon" src="<?php echo TEMPLATE_PATH; ?>/images/Ecommerce_Set-48.svg"></div>
+				      <h3 class="referral-block__title">Track progress</h3>
+				      <p class="referral-block__description">Wait until your referrals upload or purchase models</p>
+				   </div>
+				   <div class="referral-block is-last">
+				      <div class="referral-block__image"><img alt="Icon" src="<?php echo TEMPLATE_PATH; ?>/images/Ecommerce_Set-17.svg"></div>
+				      <h3 class="referral-block__title">Be Rewarded</h3>
+				      <p class="referral-block__description">Grow reputation to increase your royalty rate</p>
+				   </div>
+				   <div class="clear"></div>
+				</div>
+				<ul class="referral-rewards">
+				   <li><b>+50pts</b> for first model upload of your referral </li>
+				   <li><b>+50pts</b> for first purchase of your referral </li>
+				   <li><b>+5pts</b> for model upload of your referral </li>
+				   <li><b>+5pts</b> for purchase of your referral </li>
+				   <li><b>+3pts</b> for referral registration </li>
+				</ul>
+
+				<div class="card">
+				   <div class="card__content">
+				      <h3 class="card__heading">Share Referral Link</h3>
+				      <div class="referral-link">
+				         <div class="referral-link__field">
+				            <div class="input-container is-last"><input class="field field--spaced field--full js-referral-link" type="text" value=""></div>
+				         </div>
+				         <div class="referral-link__action">
+				            <div class="referral__share">
+				               <ul class="list list--inline u-text-center social-networks">
+				                  <li class="list__item"><a target="_blank" class="social-icon facebook" data-social-network="Facebook" data-page-title="Referral page" id="367be094c36caed1e3e890b5821033f9" href="https://www.facebook.com/sharer.php?u=https://www.cgtrader.com/join/binhdarkcu?utm_medium=Social%26utm_campaign=Share%26utm_source=Facebook"><span class="fa-stack fa-social-share fa-facebook-icon"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-facebook fa-inverse fa-stack-1x"></i></span></a></li>
+				                  <li class="list__item"><a target="_blank" class="social-icon linkedin" data-social-network="Linkedin" data-page-title="Referral page" id="556c2832fb02f13803275669abf2b149" href="http://www.linkedin.com/shareArticle?mini=true&amp;url=https://www.cgtrader.com/join/binhdarkcu?utm_medium=Social%2526utm_campaign=Share%2526utm_source=Linkedin"><span class="fa-stack fa-social-share fa-linkedin-icon"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-linkedin fa-inverse fa-stack-1x"></i></span></a></li>
+				                  <li class="list__item"><a target="_blank" class="social-icon twitter" data-social-network="Twitter" data-page-title="Referral page" id="b1bb702f08a44914bb4cd2cf8c382394" href="https://twitter.com/intent/tweet?text=Join+CGTrader+to+access+thousands+of+%233Dmodels+for+your+projects&amp;url=https%3A%2F%2Fwww.cgtrader.com%2Fjoin%2Fbinhdarkcu%3Futm_medium%3DSocial%2526utm_campaign%3DShare%2526utm_source%3DTwitter&amp;via=CG_Trader"><span class="fa-stack fa-social-share fa-twitter-icon"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-twitter fa-inverse fa-stack-1x"></i></span></a></li>
+				                  <li class="list__item"><a target="_blank" class="social-icon instagram" data-social-network="Google" data-page-title="Referral page" id="89f181e5f52f85df3b3a93e6ca1c5b96" href="https://plus.google.com/share?url=https://www.cgtrader.com/join/binhdarkcu?utm_medium=Social%26utm_campaign=Share%26utm_source=Google"><span class="fa-stack fa-social-share fa-google-plus-icon"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-google-plus fa-inverse fa-stack-1x"></i></span></a></li>
+				                  <li class="list__item is-last"><a target="_blank" class="social-icon pinterest" data-social-network="Pinterest" data-page-title="Referral page" id="d17d3aca45f8a411be027916bbceec3e" href="https://pinterest.com/pin/create/button/?url=https://www.cgtrader.com/join/binhdarkcu?utm_medium=Social%2526utm_campaign=Share%2526utm_source=Pinterest&amp;media=https://assets.cgtrader.com/assets/branding/avatar-e44f906e88b0796e0c8578ea4d155a91bcb6abdd57193aacc6d28157fcfcecd2.jpg&amp;description=Join%20the%20largest%203D%20Model%20marketplace"><span class="fa-stack fa-social-share fa-pinterest-icon"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-pinterest fa-inverse fa-stack-1x"></i></span></a></li>
+				               </ul>
+				               <script src="https://platform.twitter.com/widgets.js" async="async"></script>
+				            </div>
+				         </div>
+				         <div class="clear"></div>
+				      </div>
+				   </div>
+				</div>
+				<div class="content-heading">
+					<h2 class="content-heading__title">Performance Tracking</h2>
+					<p class="content-heading__subtitle"></p><p class="muted">It appears you haven't invited anyone just yet.</p>
+				</div>
 				
-				
-				<div class="content-heading"><h2 class="content-heading__title">Performance Tracking</h2><p class="content-heading__subtitle"></p><p class="muted">It appears you haven't invited anyone just yet.</p></div>
 			</div>
 		</section>
 	</div>
